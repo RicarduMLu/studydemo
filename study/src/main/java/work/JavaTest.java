902be5f4434537fb3da8dd2279a63eb88252bbaa
lizijian <lzj1063938106@outlook.com>
2022-06-22 17:33:29 +0800

    补充办公使用的转换测试工具

--- study/src/main/java/work/JavaTest.java
+++ study/src/main/java/work/JavaTest.java
@@ -9,8 +9,11 @@ public class JavaTest {
 
     @Test
     public void getUrls() {
+        System.out.println();
+        System.out.println();
         boolean innerFlag = false;
-        JSONObject jsonObject = JSON.parseObject(TestConstants.fileUrls1);
+//        JSONObject jsonObject = JSON.parseObject(TestConstants.fileUrls16590);
+        JSONObject jsonObject = JSON.parseObject(TestConstants.fileUrls16590免责);
         JSONObject data = (JSONObject) jsonObject.get("data");
         JSONArray parseArray = JSON.parseArray(JSON.toJSONString(data.get("fileUrlList")));
         for (int i = 0; i < parseArray.size(); i++) {
@@ -21,10 +24,61 @@ public class JavaTest {
             } else {
                 System.out.print(url.get("outerUrl"));
             }
-            if (i < parseArray.size()-1) {
+            if (i < parseArray.size() - 1) {
                 System.out.print(",");
             }
         }
+        System.out.println();
+        System.out.println();
+    }
+
+    @Test
+    public void getUrls2() {
+        System.out.println();
+        System.out.println();
+        boolean innerFlag = true;
+        JSONObject jsonObject = JSON.parseObject(TestConstants.fileTemplate2);
+//        JSONObject jsonObject = JSON.parseObject(TestConstants.fileTemplate);
+        JSONObject data = (JSONObject) jsonObject.get("data");
+        JSONArray parseArray = JSON.parseArray(JSON.toJSONString(data.get("fileUrlList")));
+        for (int i = 0; i < parseArray.size(); i++) {
+            Object fileUrlList = parseArray.get(i);
+            JSONObject url = (JSONObject) fileUrlList;
+            if (innerFlag) {
+                System.out.println("    911201_PDF_" + (i + 1) + ": " + url.get("innerUrl"));
+            } else {
+                System.out.print(url.get("outerUrl"));
+            }
+            if (i < parseArray.size() - 1) {
+            }
+        }
+        System.out.println();
+
+    }
+    @Test
+    public void getUrls3() {
+        System.out.println();
+        System.out.println();
+        boolean innerFlag = false;
+        JSONObject jsonObject = JSON.parseObject(TestConstants.fileUrl);
+//        JSONObject jsonObject = JSON.parseObject(TestConstants.fileTemplate);
+        JSONObject data = (JSONObject) jsonObject.get("data");
+        JSONArray parseArray = JSON.parseArray(JSON.toJSONString(data.get("fileUrlList")));
+        for (int i = 0; i < parseArray.size(); i++) {
+            Object fileUrlList = parseArray.get(i);
+            JSONObject url = (JSONObject) fileUrlList;
+            if (innerFlag) {
+            } else {
+                System.out.print(url.get("outerUrl") );
+                System.out.print("   ");
+                System.out.print(url.get("originalFileName") );
+            }
+            System.out.println();
+
+            if (i < parseArray.size() - 1) {
+            }
+        }
+        System.out.println();
 
     }
 }
